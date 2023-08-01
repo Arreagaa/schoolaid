342e1e34b613b13a29786d49e2ace930debaf89e
Christian Rivas <99852554+Arreagaa@users.noreply.github.com>
2023-07-31 21:11:18 -0600

    fix

--- Pages/index.vue
+++ Pages/index.vue
@@ -21,53 +21,65 @@ export default {
     SVideo,
     SEcosystems,
   },
-  mounted() {
-    window.intercomSettings = {
-      api_base: "https://api-iam.intercom.io",
-      app_id: "csz7m2co",
-    };
-
-    (function () {
-      var w = window;
-      var ic = w.Intercom;
-      if (typeof ic === "function") {
-        ic("reattach_activator");
-        ic("update", w.intercomSettings);
-      } else {
-        var d = document;
-        var i = function () {
-          i.c(arguments);
-        };
-        i.q = [];
-        i.c = function (args) {
-          i.q.push(args);
-        };
-        w.Intercom = i;
-        var l = function () {
-          var s = d.createElement("script");
-          s.type = "text/javascript";
-          s.async = true;
-          s.src = "https://widget.intercom.io/widget/csz7m2co";
-          var x = d.getElementsByTagName("script")[0];
-          x.parentNode.insertBefore(s, x);
-        };
-        if (document.readyState === "complete") {
-          l();
-        } else if (w.attachEvent) {
-          w.attachEvent("onload", l);
-        } else {
-          w.addEventListener("load", l, false);
+  watch: {
+    "$i18n.locale": {
+      immediate: true,
+      handler(newLocale) {
+        if (process.client) {
+          window.intercomSettings = {
+            api_base: "https://api-iam.intercom.io",
+            app_id: "csz7m2co",
+            language_override: newLocale,
+          };
+          if (typeof window.Intercom === "function") {
+            window.Intercom("reattach_activator");
+            window.Intercom("update", window.intercomSettings);
+          } else {
+            (function () {
+              var w = window;
+              var ic = w.Intercom;
+              if (typeof ic === "function") {
+                ic("reattach_activator");
+                ic("update", w.intercomSettings);
+              } else {
+                var d = document;
+                var i = function () {
+                  i.c(arguments);
+                };
+                i.q = [];
+                i.c = function (args) {
+                  i.q.push(args);
+                };
+                w.Intercom = i;
+                var l = function () {
+                  var s = d.createElement("script");
+                  s.type = "text/javascript";
+                  s.async = true;
+                  s.src = "https://widget.intercom.io/widget/csz7m2co";
+                  var x = d.getElementsByTagName("script")[0];
+                  x.parentNode.insertBefore(s, x);
+                };
+                if (document.readyState === "complete") {
+                  l();
+                } else if (w.attachEvent) {
+                  w.attachEvent("onload", l);
+                } else {
+                  w.addEventListener("load", l, false);
+                }
+              }
+            })();
+          }
         }
-      }
-    })();
+      },
+    },
   },
 };
 </script>
 <template>
   <section>
-    <div>
+    <client-only>
       <AppHeader />
-    </div>
+    </client-only>
     <div
       class="bg-[url('/assets/bg/Header.png')] bg-cover xl:bg-center bg-left"
     >
